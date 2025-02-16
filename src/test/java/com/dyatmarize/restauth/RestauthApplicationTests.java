0ee1f16605e900a80a22db2b9b8da44bfe93b903
dyatmarize <raihanadyatma8@gmail.com>
2025-02-16 19:33:29 +0700

    Initial Commit, Prep Repository

---
+++ src/test/java/com/dyatmarize/restauth/RestauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyatmarize.restauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
